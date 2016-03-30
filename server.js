d4ad8c0f7a968218c3dd776afc5f14e37831ffac
Manoj Kumar <mano.mykingdom@gmail.com>
2016-03-30 18:12:17 +0530

    integrated log4js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 
+var log4js = require('log4js');
+log4js.configure({
+    appenders: [{
+        type: 'console'
+    }, {
+        type: 'file',
+        filename: './logs/ErrorHandling.log'
+    }]
+});
+
+var logger = log4js.getLogger('ErrorHandling');
+
 var _ = require('underscore');
 
 var bodyParser = require('body-parser');
@@ -12,11 +24,11 @@ app.use(bodyParser.urlencoded({
 var knex = require('knex')({
     client: 'pg',
     connection: {
-        host     : '127.0.0.1',
-        user     : 'postgres',
-        password : '1',
-        database : 'users_directory',
-        charset  : 'utf8'
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '1',
+        database: 'users_directory',
+        charset: 'utf8'
     }
 });
 var bookshelf = require('bookshelf')(knex);
@@ -24,14 +36,25 @@ var User = bookshelf.Model.extend({
     tableName: 'users'
 });
 
-function success(res, model){
+function success(req, res, next) {
+    var model = req.model;
+    if (!model) {
+        return next(new Error('We are sorry, error occured.'));
+    }
     res.json({
         success: true,
         data: model.toJSON()
     });
 }
 
-function error(res, err){
+function error(err, req, res, next) {
+    logger.trace('*****************');
+    logger.debug('path', req.path);
+    logger.debug('method', req.method);
+    logger.debug('query', req.query);
+    logger.debug('body', req.body);
+    logger.error(err);
+    logger.trace('*****************');
     res.json(_.defaults(_.pick(err, ['code', 'message']), {
         success: false
     }));
@@ -40,48 +63,46 @@ function error(res, err){
 //route app path
 app.use('/', express.static(__dirname + "/app"));
 
-app.get('/users', function (req, res) {
-    User.fetchAll().then(function(model){
-        success(res, model);
-    }).catch(function(err){
-        error(res, err);
-    });
+app.get('/users', function (req, res, next) {
+    User.fetchAll().then(function (model) {
+        req.model = model;
+        next();
+    }).catch(next);
 });
 
-app.get('/user/:id', function (req, res) {
+app.get('/user/:id', function (req, res, next) {
     User.where('id', req.params.id).fetch({
         require: true
-    }).then(function(model){
-        success(res, model);
-    }).catch(function(err){
-        error(res, err);
-    });
+    }).then(function (model) {
+        req.model = model;
+        next();
+    }).catch(next);
 });
 
-app.post('/user/create', function (req, res) {
-    new User(_.pick(req.body, ['fname', 'lname', 'age'])).save().then(function(model){
-        success(res, model);
-    }).catch(function(err){
-        error(res, err);
-    });
+app.post('/user/create', function (req, res, next) {
+    new User(_.pick(req.body, ['fname', 'lname', 'age'])).save().then(function (model) {
+        req.model = model;
+        next();
+    }).catch(next);
 });
 
-app.post('/user/update', function (req, res) {
-    new User(_.pick(req.body, ['id', 'fname', 'lname', 'age'])).save().then(function(model){
-        success(res, model);
-    }).catch(function(err){
-        error(res, err);
-    });
+app.post('/user/update', function (req, res, next) {
+    new User(_.pick(req.body, ['id', 'fname', 'lname', 'age'])).save().then(function (model) {
+        req.model = model;
+        next();
+    }).catch(next);
 });
 
-app.delete('/user/destroy/:id', function (req, res) {
+app.delete('/user/destroy/:id', function (req, res, next) {
     User.where('id', req.params.id).destroy({
         require: true
-    }).then(function(model){
-        success(res, model);
-    }).catch(function(err){
-        error(res, err);
-    });
+    }).then(function (model) {
+        req.model = model;
+        next();
+    }).catch(next);
 });
+
+app.use(success);
+app.use(error);
 
 app.listen(3000);
